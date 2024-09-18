4e57019a0967b82ecf5dfacdeebdfc84d0585295
Teresa Rosa <ter.af.rosa@gmail.com>
2024-09-18 23:54:28 +0100

    expressões matemáticas em Latex

--- main.py
+++ main.py
@@ -29,9 +29,13 @@ def calcular_expressao(expressao, latex=False):
             expressao_limpa = limpar_expressao(expressao)
             logging.info(f"Expressão após limpeza: {expressao_limpa}")  # Log após a limpeza
 
+            # Decodificar barras invertidas
+            expressao_decodificada = bytes(expressao_limpa, "utf-8").decode("unicode_escape")
+            logging.info(f"Expressão após decodificação: {expressao_decodificada}")  # Log após a decodificação
+
             # Use a expressão como uma string "raw" para evitar problemas de barras invertidas
             try:
-                sympy_expr = latex2sympy.latex2sympy(rf"{expressao_limpa}")
+                sympy_expr = latex2sympy.latex2sympy(expressao_decodificada)
                 logging.info(f"Expressão convertida para SymPy: {sympy_expr}")  # Log antes da avaliação
             except Exception as e:
                 logging.error(f"Erro ao converter LaTeX para SymPy: {str(e)}")
