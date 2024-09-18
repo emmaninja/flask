0cb294ae8fad1308acc75db325178113918c9841
Teresa Rosa <ter.af.rosa@gmail.com>
2024-09-18 22:16:52 +0100

    expressões matemáticas em Latex

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 import sympy as sp
-import latex2sympy2 as latex2sympy  # Ajustando a importação para latex2sympy
+import latex2sympy2 as latex2sympy
 import os
 
 app = Flask(__name__)
@@ -8,6 +8,8 @@ app = Flask(__name__)
 def calcular_expressao(expressao, latex=False):
     try:
         if latex:
+            # Remover escapes adicionais da expressão
+            expressao = expressao.replace('\\\\', '\\')  # Substitui '\\' por '\'
             # Processar LaTeX usando latex2sympy
             sympy_expr = latex2sympy.latex2sympy(expressao)
         else:
