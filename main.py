d5387e42410f57f63b5e69b3ab821b53523c43b6
Teresa Rosa <ter.af.rosa@gmail.com>
2024-09-18 17:53:58 +0100

    expressões matemáticas em Latex

--- main.py
+++ main.py
@@ -1,31 +1,37 @@
 from flask import Flask, request, jsonify
 import sympy as sp
 from sympy.parsing.latex import parse_latex
+import numpy as np
 import os
 
 app = Flask(__name__)
 
-# Função para cálculos matemáticos complexos usando SymPy
-def calcular_expressao_complexa(expressao, latex=False):
+# Função para calcular a expressão
+def calcular_expressao(expressao, latex=False):
     try:
-        # Se a expressão estiver em LaTeX, usar o parseador de LaTeX do SymPy
+        # Se a expressão estiver em LaTeX, converter para uma expressão SymPy
         if latex:
             sympy_expr = parse_latex(expressao)
         else:
-            sympy_expr = sp.sympify(expressao)  # Converte a expressão para uma forma simbólica
+            sympy_expr = sp.sympify(expressao)
         
-        # Cálculo específico, como limites
+        # Avaliar o tipo de operação a ser realizada
         if sympy_expr.has(sp.Limit):
+            # Calcular o limite se a expressão contiver um objeto de limite
             resultado = sympy_expr.doit()
         else:
-            # Tentativa de simplificar a expressão com SymPy
+            # Tentar simplificar a expressão
             resultado = sp.simplify(sympy_expr)
         
-        print(f"Resultado: {resultado}")
+        # Verificar se a expressão simplificada é um número
+        if resultado.is_number:
+            # Avaliação numérica
+            resultado = resultado.evalf()
+        
+        # Retornar o resultado
         return resultado
     except Exception as e:
-        print(f"Erro ao processar a expressão: {e}")
-        return str(e)
+        return f"Erro ao processar a expressão: {str(e)}"
 
 @app.route('/')
 def index():
@@ -34,7 +40,6 @@ def index():
 @app.route('/calcular', methods=['POST'])
 def calcular():
     dados = request.json
-    print(f"Dados recebidos: {dados}")
     expressao = dados.get('expressao', '')
     latex = dados.get('latex', False)
 
@@ -42,15 +47,11 @@ def calcular():
         return jsonify({'erro': 'Nenhuma expressão fornecida'}), 400
 
     try:
-        resultado = calcular_expressao_complexa(expressao, latex=latex)
-        print(f"Resultado do cálculo: {resultado}")
+        # Processar a expressão
+        resultado = calcular_expressao(expressao, latex=latex)
         return jsonify({'resultado': str(resultado)})
-    except ValueError as ve:
-        print(f"Erro de valor: {ve}")
-        return jsonify({'erro': str(ve)}), 400
     except Exception as e:
-        print(f"Erro ao processar a expressão: {e}")
-        return jsonify({'erro': 'Erro ao processar a expressão'}), 500
+        return jsonify({'erro': f'Erro ao processar a expressão: {str(e)}'}), 500
 
 if __name__ == '__main__':
     port = int(os.getenv("PORT", 8080))
