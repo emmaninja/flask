b6c6d215347bec52c6ca3526e828a8524e5783da
Teresa Rosa <ter.af.rosa@gmail.com>
2024-09-19 14:59:32 +0100

    expressões matemáticas em Latex

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from flask import Flask, request, jsonify
 import sympy as sp
 import latex2sympy2 as latex2sympy
-import os
 import logging
 
 app = Flask(__name__)
@@ -9,43 +8,28 @@ app = Flask(__name__)
 # Configurar o logger
 logging.basicConfig(level=logging.INFO)
 
-def calcular_expressao(expressao, latex=False):
+def calcular_expressao(expressao):
     try:
-        # Remover duplicações de barras invertidas antes do processamento
-        expressao = expressao.replace('\\\\', '\\')
-        logging.info(f"Expressão após substituir barras invertidas duplas: {expressao}")
+        # Log da expressão recebida
+        logging.info(f"Expressão LaTeX recebida: {expressao}")
 
-        if latex:
-            # Processar LaTeX usando latex2sympy
-            expressao = expressao.replace('\\\\\\', '\\')
-            sympy_expr = latex2sympy.latex2sympy(expressao)
-            logging.info(f"Expressão convertida para SymPy: {sympy_expr}")
-        else:
-            expressao = expressao.replace('\\\\\\', '\\')
-            sympy_expr = sp.sympify(expressao)
+        # Processar LaTeX usando latex2sympy
+        sympy_expr = latex2sympy.latex2sympy(expressao)
+        logging.info(f"Expressão convertida para SymPy: {sympy_expr}")
 
-        # Avaliar o tipo de operação a ser realizada
-        if isinstance(sympy_expr, sp.Basic):
-            # Tentar simplificar a expressão
-            resultado = sp.simplify(sympy_expr)
-        else:
-            # Avaliação numérica
-            resultado = sympy_expr.evalf()
+        # Avaliar a expressão
+        resultado = sp.simplify(sympy_expr)
+        logging.info(f"Resultado da expressão: {resultado}")
 
         return resultado
     except Exception as e:
         logging.error(f"Erro ao processar a expressão: {str(e)}")
         return f"Erro ao processar a expressão: {str(e)}"
 
-@app.route('/')
-def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
-
 @app.route('/calcular', methods=['POST'])
 def calcular():
     dados = request.json
     expressao = dados.get('expressao', '')
-    latex = dados.get('latex', False)
 
     # Log da entrada original
     logging.info(f"Entrada original recebida: {dados}")
@@ -55,12 +39,11 @@ def calcular():
 
     try:
         # Processar a expressão
-        resultado = calcular_expressao(expressao, latex=latex)
+        resultado = calcular_expressao(expressao)
         return jsonify({'resultado': str(resultado)})
     except Exception as e:
         logging.error(f"Erro ao processar a expressão: {str(e)}")
         return jsonify({'erro': f'Erro ao processar a expressão: {str(e)}'}), 500
 
 if __name__ == '__main__':
-    port = int(os.getenv("PORT", 8080))
-    app.run(debug=os.getenv("FLASK_DEBUG", "false").lower() == "true", host='0.0.0.0', port=port)
+    app.run(debug=True, host='0.0.0.0', port=8080)
